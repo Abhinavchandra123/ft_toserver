0d6ec3ba1c0c972d2c4e40e4738c1a2efc2a57fe
Abhinavchandra123 <96311051+Abhinavchandra123@users.noreply.github.com>
2024-08-10 19:31:12 +0530

    Update product.py

--- product.py
+++ product.py
@@ -22,7 +22,7 @@ class RCKongenCrawler:
         # ****************** Use this for server hosting ***********************
         options.add_argument("--headless") 
         options.add_argument("--disable-gpu")
-        # options.add_argument("--no-sandbox")
+        options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         # **********************************************************************
         
